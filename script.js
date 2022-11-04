4273a78b9120045d7e26588d35168d702e6d5788
megta <113287791+megta@users.noreply.github.com>
2022-11-04 22:41:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const accordion = document.querySelectorAll('.card__gen-container');
 
 for (let i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener('click', () => {
+    accordion[i].addEventListener('click', function () {
         this.classList.toggle('active');
     });
 }
