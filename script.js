4882f61bb251b09d2e8a16e13ac62431a78a0f9d
megta <113287791+megta@users.noreply.github.com>
2022-11-06 23:02:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// why loop on .card__gen-container instead of .card__question?
+
+
 const accordion = document.querySelectorAll('.card__gen-container');
 
 for (let i = 0; i < accordion.length; i++) {
