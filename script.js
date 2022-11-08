e4f8db4886c6c59dfa3f792e49bf755f4fb4772e
megta <113287791+megta@users.noreply.github.com>
2022-11-08 16:25:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 'use strict';
 
-// make accordion items open one at a time
-
-
 const accordion = document.querySelectorAll('.card__gen-container');
-
-/*
-for (let i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener('click', function () {
-        this.classList.toggle('active');
-    });
-}
-*/
 
 accordion.forEach((item) => {
 item.addEventListener('click', function() {
     accordion.forEach((elem) => {
         if(elem.classList.contains('active')) {
             elem.classList.toggle('active');
-        } else if(elem === item) {
+        } else if (elem === item) {
             elem.classList.toggle('active');
         }
     });
 });
 });
+
+// for when you want to open multiple at a time
+
+/* 
+for (let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+    });
+}
+*/
