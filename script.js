e0f00d2f0dd4fe10115e2cb21154327901e0b2ef
megta <113287791+megta@users.noreply.github.com>
2022-11-08 16:02:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,22 @@
 
 const accordion = document.querySelectorAll('.card__gen-container');
 
+/*
 for (let i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener('click', function () {
         this.classList.toggle('active');
     });
 }
+*/
 
+accordion.forEach((item) => {
+item.addEventListener('click', function() {
+    accordion.forEach((elem) => {
+        if(elem.classList.contains('active')) {
+            elem.classList.toggle('active');
+        } else if(elem === item) {
+            elem.classList.toggle('active');
+        }
+    });
+});
+});
